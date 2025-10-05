b499dd776c061b3ba0e82890820c7b53ea1cb836
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-10-05 10:50:32 +0100

    Add files created

---
+++ movieapp/src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </div>
+  );
+}

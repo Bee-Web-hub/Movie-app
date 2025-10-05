c43d85add95f59cdbed28b9c9b3d9f968b3fe968
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-10-05 11:34:15 +0100

    Change of name from Movieapp to moviehub

--- movieapp/vite.config.js
+++ movieapp/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  define: {
+    __APP_NAME__: JSON.stringify('MovieHub'),
+  },
 })
